5c9e4bf72846e2a78dcfc4f03d8da5c7d675d3ff
Georgi Hristozov <georgi@forkbomb.nl>
2011-01-26 23:10:33 +0200

    fill the polygon buffer BEFORE calculating vertex normals

--- buffer.c
+++ buffer.c
@@ -88,10 +88,10 @@ void add_point(float x, float y, float z) {
 		//printf("POINT: %f %f %f DISTANCE FROM 0: %f\n", vertex_buffer[cur_index][i].x, vertex_buffer[cur_index][i].y, vertex_buffer[cur_index][i].z, sqrtf(vertex_buffer[cur_index][i].x*vertex_buffer[cur_index][i].x + vertex_buffer[cur_index][i].y*vertex_buffer[cur_index][i].y + vertex_buffer[cur_index][i].z*vertex_buffer[cur_index][i].z));
 	}
 	
+	fill_polygon_buffer();
 	#ifdef SMOOTH_SHADING
 		calculate_vertex_normals();
 	#endif
-	fill_polygon_buffer();
 }
 
 /* Добавя указател към полигон в списъка с указатели на дадения връх */
