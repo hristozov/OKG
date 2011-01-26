12a2a58fff35b2f2d893168ed70292cc77216009
Georgi Hristozov <hristozov@gmail.com>
2011-01-26 18:20:51 +0200

    useless comment

--- normal.h
+++ normal.h
@@ -4,8 +4,6 @@
 #include "buffer.h"
 #include "globals.h"
 
-//extern size_t no_segments;
-
 void calculateNormal(struct point*, struct point*, struct point*, struct point*);
 void vertexNormal(size_t, size_t);
 void calculateVertexNormals();
