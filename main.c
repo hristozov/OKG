e4bb5b4d69cd1417e884edcf3bd5e95e47a0699d
Georgi Hristozov <georgi@forkbomb.nl>
2011-01-28 20:45:49 +0200

    use switch in the keyboard() function

--- main.c
+++ main.c
@@ -146,14 +146,13 @@ void mouse (int button, int state, int mx, int my) {
 
 /* Callback за клавиатурата. Използва се за въртене. */
 void keyboard (int key, int x, int y) {
-	if (key == GLUT_KEY_UP)
-		diff_x -= 15;
-	if (key == GLUT_KEY_DOWN)
-		diff_x += 15;
-	if (key == GLUT_KEY_LEFT)
-		diff_y -= 15;
-	if (key == GLUT_KEY_RIGHT)
-		diff_y += 15;
+	switch(key) {
+		case GLUT_KEY_UP:    diff_x -=15; break;
+		case GLUT_KEY_DOWN:  diff_x +=15; break;
+		case GLUT_KEY_LEFT:  diff_y -=15; break;
+		case GLUT_KEY_RIGHT: diff_x +=15; break;
+	}
+	glutPostRedisplay();
 }
 
 /* Callback за смяна на размера на прозореца */
